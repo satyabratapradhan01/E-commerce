32cf5da7e1ad373cf1aa36218b25eb5ef72ce417
satyabratapradhan <satyabratapradhann@gmail.com>
2025-07-17 23:58:15 +0530

    env

---
+++ backend/controllers/productController.js
@@ -0,0 +1,25 @@
+
+
+
+// function for add product
+const addProduct = async(req, res) => {
+
+}
+
+// function for list product
+const listProduct = async(req, res) => {
+
+}
+
+// function for removing product
+const removeProduct = async(req, res) => {
+
+}
+
+// function for single product info
+const singleProduct = async (req, res) => {
+
+}
+
+
+export {listProduct, addProduct, removeProduct, singleProduct}
