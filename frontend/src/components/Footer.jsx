8f6a797d3a336ad0147c9c4cc676d77ba044b809
satyabratapradhan <satyabratapradhann@gmail.com>
2025-09-07 00:40:11 +0530

    satya

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,10 +6,9 @@ const Footer = () => {
     <div>
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             <div>
-                <img src={assets.logo} alt="" />
+                <img src={assets.logo} className='w-44' alt="" />
                 <p className='w-full md:w-2/3 text-gray-600'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quo officia facere velit quos, incidunt unde tenetur hic odio ullam amet architecto ratione perspiciatis, cumque et blanditiis nulla a! Ullam.
-                </p>
+we’re a lifestyle. Our mission is to bring you high-quality, trendy, and affordable fashion that fits every occasion.                </p>
             </div>
             <div>
                 <p className='text-xl font-medium mb-5'>COMPANY</p>
