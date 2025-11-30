ea7f2b8071a0f757a4e9b49e845cae34db9b7769
satyabratapradhan <satyabratapradhann@gmail.com>
2025-12-01 01:37:13 +0530

    chenge the css

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -57,7 +57,7 @@ const List = ({token}) => {
 
         {
           list.map((item, index) => (
-            <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_7fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
+            <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
               <img src={item.image[0]} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
